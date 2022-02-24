b04689e661b1bd27b5bc5da15c84f95cb078b911
edward-coombes-nuodb <edward.coombes@3ds.com>
2022-02-24 11:44:44 -0500

    Add test for callback style

---
+++ test/1.callback.js
@@ -0,0 +1,53 @@
+// Copyright (c) 2018-2019, NuoDB, Inc.
+// All rights reserved.
+//
+// Redistribution and use permitted under the terms of the 3-clause BSD license.
+
+'use strict';
+
+var { Driver } = require('..');
+
+var should = require('should');
+var config = require('./config.js');
+
+describe('1. testing callback', () => {
+
+  var driver = null;
+
+  before('create driver', function () {
+    driver = new Driver();
+  });
+
+  it('1.1 open and close connections using callbacks', function (done) {
+    driver.connect(config, (err, conn) => {
+      should.not.exist(err);
+      const connection = conn;
+      connection.should.be.ok();
+      connection.close((err) => {
+        should.not.exist(err);
+        done();
+      })
+      connection.should.be.ok();
+    })
+  });
+
+  it('1.2 can run the sample', function (done) {
+    driver.connect(config, (err, conn) => {
+      should.not.exist(err);
+      const connection = conn;
+      connection.should.be.ok();
+      connection.execute('SELECT 1 AS VALUE FROM DUAL', (err, results) => {
+        should.not.exist(err);
+        results.should.be.ok();
+        results.getRows((err,rows) => {
+          should.not.exist(err);
+          rows.should.be.ok();
+          connection.close((err) => {
+            should.not.exist(err);
+            done();
+          })
+        });
+      });
+    })
+  });
+});
