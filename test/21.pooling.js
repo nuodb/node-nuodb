967602025b94449017479f3747156352b1c6404e
ROSENBERG Philip <PRG1@dsone.3ds.com>
2022-04-11 14:23:04 -0400

    removed debugging console.log, test is working, I forgot to use "await" with async pool methods

--- test/21.pooling.js
+++ test/21.pooling.js
@@ -93,7 +93,6 @@ describe("13. test pooling", () => {
   it("Does not close a connection in use on age out", async () => {
     let curr = await pool.requestConnection();
     await pool._closeConnection(curr.id);
-    console.log("CHECK THIS OUT>>>>>>>", curr, pool.all_connections[curr.id]);
     should.equal(
       pool.all_connections[curr.id].ageStatus,
       true,
