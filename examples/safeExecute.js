a9fa1aa75131da3ccd8fd98df294fd6f51daee54
Edward Coombes <ecs8@3ds.com>
2022-06-06 11:17:48 -0400

    change prework to prepwork, add handling for null prepwork

--- examples/safeExecute.js
+++ examples/safeExecute.js
@@ -1,11 +1,11 @@
-const safeExecute = async (retryLimit, work, prework, mutables) => {
+const safeExecute = async (retryLimit, work, prepwork, mutables) => {
   let success = false;
   let result = null;
   let err = null;
   for(let i = 0;i < retryLimit && !success; i++) {
     try {
-      const preworkResults = await prework(mutables, err);
-      result = await work(preworkResults ?? mutables);
+      const prepworkResults = await prepwork?.call(mutables, err);
+      result = await work(prepworkResults ?? mutables);
       success = true;
     } catch (e) {
       err = e;
