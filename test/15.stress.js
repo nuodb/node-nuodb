415479af46661a703396f59792521402609a2d05
Edward Coombes <ecs8@3ds.com>
2022-07-27 15:40:42 -0400

    add tracking of results of query to the test

--- test/15.stress.js
+++ test/15.stress.js
@@ -32,6 +32,23 @@ const sleep = (ms) => new Promise((res) => {
   setTimeout(() => res(),ms)
 } );
 
+const mean = (numbers) => numbers.reduce((acc,val)=>(acc+val/numbers.length),0);
+const median = (numbers) => {
+    const sorted = Array.from(numbers).sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+
+    if (sorted.length % 2 === 0) {
+        return (sorted[middle - 1] + sorted[middle]) / 2;
+    }
+
+    return sorted[middle];
+}
+const stddev = (numbers, knownMean=undefined) => {
+  const meanVal = knownMean ?? mean(numbers);
+  const variance = numbers.reduce((acc,val) => acc + (val-meanVal) ** 2, 0) / (numbers.length - 1)
+  return Math.sqrt(variance);
+}
+
 const second = 1000;
 const minute = 60 * second;
 
@@ -92,19 +109,160 @@ const randomInsertQueries = () => {
   return q;
 }
 
+const fakeWork = (numberOfOperations, operationalLoad) => new Promise( res => {
+  for(let i = 0; i < numberOfOperations; i++){
+    const arr = []
+    for(let j = 0; j < operationalLoad; j++)
+      arr.push(rand(0,1000))
+    stddev(arr)
+  }
+
+  // we need to let the stack clear and put this in the task queue
+  setTimeout(() => res(), 0)
+})
+
 const TEST_CASE_DURATION = 2 * minute;
-const testCase = (description, concurrency, time, getQuery) => ({description, concurrency,time, getQuery})
 const CS_TEST_CASES = [
-  testCase('select sys tables', 100, TEST_CASE_DURATION, selectSysTablesQuery),
-  testCase('select sys tables', 500, TEST_CASE_DURATION, selectSysTablesQuery),
-  testCase('random read', 100, TEST_CASE_DURATION, randomReadQueriesWithWork),
-  testCase('random read', 500, TEST_CASE_DURATION, randomReadQueriesWithWork),
-  testCase('random insert', 100, TEST_CASE_DURATION, randomInsertQueries),
-  testCase('random insert', 500, TEST_CASE_DURATION, randomInsertQueries),
-  testCase('long working', 100, TEST_CASE_DURATION, longWorkingQuery),
-  testCase('long working', 500, TEST_CASE_DURATION, longWorkingQuery),
-  testCase('msleep', 100, TEST_CASE_DURATION, msleep2500Query),
-  testCase('msleep', 500, TEST_CASE_DURATION, msleep2500Query),
+  {
+    description: 'select sys tables',
+    concurrency: 100,
+    time: TEST_CASE_DURATION,
+    getQuery: selectSysTablesQuery,
+    expectedResults: {
+      totalExpected:28200,
+      totalDelta:1000,
+      meanExpected:425,
+      stdDevExpected:70,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'select sys tables',
+    concurrency: 500,
+    time: TEST_CASE_DURATION,
+    getQuery: selectSysTablesQuery,
+    expectedResults: {
+      totalExpected:26366,
+      totalDelta:1000,
+      meanExpected:2297,
+      stdDevExpected:500,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'random read',
+    concurrency: 100,
+    time: TEST_CASE_DURATION,
+    getQuery: randomReadQueriesWithWork,
+    expectedResults: {
+      totalExpected:23826,
+      totalDelta:1000,
+      meanExpected:504,
+      stdDevExpected:74,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'random read',
+    concurrency: 500,
+    time: TEST_CASE_DURATION,
+    getQuery: randomReadQueriesWithWork,
+    expectedResults: {
+      totalExpected:17907,
+      totalDelta:1000,
+      meanExpected:3370,
+      stdDevExpected:1022,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'random insert',
+    concurrency: 100,
+    time: TEST_CASE_DURATION,
+    getQuery: randomInsertQueries,
+    expectedResults: {
+      totalExpected:30465,
+      totalDelta:1000,
+      meanExpected:394,
+      stdDevExpected:137,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'random insert',
+    concurrency: 500,
+    time: TEST_CASE_DURATION,
+    getQuery: randomInsertQueries,
+    expectedResults: {
+      totalExpected:29000,
+      totalDelta:1000,
+      meanExpected:2077,
+      stdDevExpected:992,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'long working',
+    concurrency: 100,
+    time: TEST_CASE_DURATION,
+    getQuery: longWorkingQuery,
+    expectedResults: {
+      totalExpected:3195,
+      totalDelta:1000,
+      meanExpected:3769,
+      stdDevExpected:943,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'long working',
+    concurrency: 500,
+    time: TEST_CASE_DURATION,
+    getQuery: longWorkingQuery,
+    expectedResults: {
+      totalExpected:2997,
+      totalDelta:1000,
+      meanExpected:20695,
+      stdDevExpected:6568,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'msleep',
+    concurrency: 100,
+    time: TEST_CASE_DURATION,
+    getQuery: msleep2500Query,
+    expectedResults: {
+      totalExpected:4716,
+      totalDelta:1000,
+      meanExpected:2575,
+      stdDevExpected:138,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
+  {
+    description: 'msleep',
+    concurrency: 500,
+    time: TEST_CASE_DURATION,
+    getQuery: msleep2500Query,
+    expectedResults: {
+      totalExpected:22251,
+      totalDelta:1000,
+      meanExpected:2726,
+      stdDevExpected:400,
+    },
+    fakeWorkLoad: 100,
+    fakeWorkComplexity: 25,
+  },
 ]
 
 const STRESS_TEST_TIMEOUT_BUFFER = TEST_CASE_DURATION / 2 * CS_TEST_CASES.length;
@@ -117,32 +275,45 @@ const promiseWhile = async (id, condition, action) => {
   }
 }
 
-const runCS = async (pool, query) => {
+const runCS = async (pool, query, elapsedTimes) => {
   try {
+    const start = Date.now();
     const conn = await pool.requestConnection();
     const results = await conn.execute(query);
     const rows = await results?.getRows();
     rows?.should.be.ok();
     await results?.close();
     await pool.releaseConnection(conn);
+    elapsedTimes.push(Date.now() - start);
   } catch (e) {
-    should.not.exist(e);
     console.error(e);
+    should.not.exist(e);
   }
 }
 
-const CSDriver = async (pool, numCS, timeToRun, getQuery) => {
+const CSDriver = async (pool, numCS, timeToRun, getQuery, fakeWorkLoad=0, fakeWorkComplexity=0) => {
 
   const csPromises = []
+  const elapsedTimes = []
   let stop = false;
+  let fakeWorkPromise = Promise.resolve();
 
   for( let i = 0; i < numCS; i++){
     const csPromise = promiseWhile(
       i,
       () => !stop, // don't stop until stop is set to true
-      () => runCS(pool, getQuery()) // every time the promise resolves, queue up another
+      () => runCS(pool, getQuery(), elapsedTimes) // every time the promise resolves, queue up another
     );
     csPromises.push(csPromise);
+  }
+
+  if(fakeWorkLoad > 0 && fakeWorkComplexity > 0){
+    // if fake work args are passed, run a fake workload in the background
+    fakeWorkPromise = promiseWhile(
+      0,
+      () => !stop,
+      () => fakeWork(fakeWorkLoad,fakeWorkComplexity)
+    )
   }
 
   // stop the promise loops after a time, and wait for them all to resolve before continuing.
@@ -150,6 +321,8 @@ const CSDriver = async (pool, numCS, timeToRun, getQuery) => {
     stop = true;
   }, timeToRun);
   await Promise.all(csPromises);
+  await fakeWorkPromise;
+  return elapsedTimes;
 }
 
 describe('15. Test Performance Under Load', async () => {
@@ -163,6 +336,7 @@ describe('15. Test Performance Under Load', async () => {
    */
   let pool = null;
   let threadCount = process.env.UV_THREADPOOL_SIZE;
+  const resultsSummary = [];
   before('init pool, create temp tables', async () => {
     // create the pool
     try{
@@ -180,6 +354,7 @@ describe('15. Test Performance Under Load', async () => {
 
   after('drop temp tables, init pool', async () => {
     try{
+      console.table(resultsSummary);
       const c = await pool.requestConnection();
       for(let i = 0; i < dropTmpTables.length; i++)
         await c.execute(dropTmpTables[i]);
@@ -191,20 +366,53 @@ describe('15. Test Performance Under Load', async () => {
   });
 
   await async.series(CS_TEST_CASES.map((curr, index) => new Promise((res) => {
-    const {description, concurrency, time, getQuery} = curr;
+    const {description, concurrency, time, getQuery,expectedResults, fakeWorkLoad, fakeWorkComplexity} = curr;
     const title = `15.${index} Testing ${concurrency} concurrent ${description} requests for ${time/minute} minutes with ${threadCount} threads`;
     describe(title, async () => {
       it(title, async () => {
         let err = null;
+        let elapsedTimes = null;
         try {
-          await CSDriver(pool, concurrency, time, getQuery);
+          // elapsedTimes in ms
+          elapsedTimes = await CSDriver(pool, concurrency, time, getQuery, fakeWorkLoad, fakeWorkComplexity);
         } catch (e) {
           err = e;
         }
         should.not.exist(err);
-        await sleep(30 * second);
+
+        const totalExecutes = elapsedTimes.length;
+        const avgExecTime = mean(elapsedTimes);
+        const medianExecTime = median(elapsedTimes);
+        const stddevExecTime = stddev(elapsedTimes, avgExecTime);
+        const numUpperOutliers = elapsedTimes.reduce((acc,curr) => acc + (curr > avgExecTime + 2 * stddevExecTime ? 1 : 0), 0);
+        const numLowerOutliers = elapsedTimes.reduce((acc,curr) => acc + (curr < avgExecTime - 2 * stddevExecTime ? 1 : 0), 0);
+        const minExecTime = Math.min(...elapsedTimes);
+        const maxExecTime = Math.max(...elapsedTimes);
+        resultsSummary.push({
+          title,
+          totalExecutes,
+          avgExecTime,
+          medianExecTime,
+          stddevExecTime,
+          numLowerOutliers,
+          numUpperOutliers,
+          minExecTime,
+          maxExecTime
+        });
+
+        // compare to historical data
+        (totalExecutes).should.be.aboveOrEqual(expectedResults.totalExpected - expectedResults.totalDelta);
+        (avgExecTime).should.be.belowOrEqual(expectedResults.meanExpected + 2 * expectedResults.stdDevExpected);
+
+        // we should not have a skewed exec time
+        (medianExecTime).should.be.approximately(avgExecTime, stddevExecTime*2);
+
+        // in a normal distribution, 95% of results should fall within 2 stddevs of norm
+        (numUpperOutliers).should.be.belowOrEqual(totalExecutes * 0.25);
+        (numLowerOutliers).should.be.belowOrEqual(totalExecutes * 0.25);
+
         res();
-      }).timeout(STRESS_TEST_TIMEOUT);
+      }).timeout(TEST_CASE_DURATION + STRESS_TEST_TIMEOUT_BUFFER);
     });
   })));
 
