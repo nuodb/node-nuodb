d7b497804b5bcaa4319744abf030be094fc65b45
Don White <dwe5@3ds.com>
2025-10-20 11:09:30 -0400

    Fix some additional cleanup as working through a rebase (#92)
    * changes for 26x, express server, reset for pooled connections, continued support for Node 22, default release build
    
    * Update rest.js
    
    Make reference to statsDictionary more generic, by using info, which represents the fact it could be stats and/or could be configuration information
    
    * Added some code to the example for multistatement transaction
    
    * fix files from a bad merge

--- examples/query.js
+++ examples/query.js
@@ -9,9 +9,9 @@ const { Driver } = require('..');
 const config = require('../test/config');
 
 var driver = new Driver();
-
 driver.connect(config)
   .then((conn) => {
+    conn.autoCommit = false;
     conn.execute('select tablename from system.tables')
       .then((results) => {
         results.getRows()
@@ -19,18 +19,34 @@ driver.connect(config)
             console.log(rows);
           });
       });
+    conn.commit();
   });
 
 // async variation
-/*
-async function query(conn, sql) {
-  let results = await conn.execute(sql);
+async function query(conn, sql, params) {
+  let results = await conn.execute(sql,params);
   return await results.getRows();
 }
 
 (async () => {
   let conn = await driver.connect(config);
-  let rows = await query(conn, 'select tablename from system.tables');
-  console.log(rows);
+  console.log(conn.autoCommit);
+  conn.autoCommit = true;
+  console.log(conn.autoCommit);
+  await conn.execute("start transaction");
+  let results = await conn.execute('select tablename from system.tables limit 2');
+  let rows = await results.getRows();
+  for (let i = 0; i < rows.length; i++) {
+    let results1 = await conn.execute("select field from system.fields where tablename = ? limit 2",[rows[i].TABLENAME]);
+    let rows1 = await results1.getRows();
+    for (let j = 0; j < rows1.length; j++) {
+      console.log(rows[i].TABLENAME,rows1[j].FIELD);
+    }
+    results1.close();
+  }
+  results.close();
+   conn.commit();
+//  await conn.execute("commit");
+  console.log(conn.autoCommit);
+  conn.close();
 })()
-*/
